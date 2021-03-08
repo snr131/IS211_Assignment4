4e6d8797264f596c75407e8d8a704224a8804b5f
snr131 <53094202+snr131@users.noreply.github.com>
2021-03-07 19:27:07 -0500

    initial commit

--- search_compare.py
+++ search_compare.py
@@ -1,34 +1,133 @@
 import argparse
-# other imports go here
-
 import random
+import timeit
+from timeit import Timer
 
 def get_me_random_list(n):
-    """Generate list of n elements in random order
+  a_list = random.sample(range(n), 500)
+  random.shuffle(a_list)
+  return a_list
+
+t1 = get_me_random_list(500)
+t2 = get_me_random_list(1000)
+t3 = get_me_random_list(5000)
     
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
-    a_list = list(range(n))
-    random.shuffle(a_list)
-    return a_list
-    
-def sequential_search(a_list,item):
-    pass
+def sequential_search(a_list, item):
+  pos = 0
+  found = False
+
+  while pos < len(a_list) and not found:
+    if a_list[pos] == item:
+      found = True
+    else:
+      pos = pos + 1
+
+  return found
 
 
-def ordered_sequential_search(a_list,item):
-    pass
+def ordered_sequential_search(a_list, item):
+  pos = 0
+  found = False
+  stop = False
+  while pos < len(a_list) and not found and not stop:
+    if a_list[pos] == item:
+      found = True
+    else:
+      if a_list[pos] > item:
+        stop = True
+      else:
+        pos = pos+1
+  
+  a_list = a_list.sort()
+
+  return found
 
 
-def binary_search_iterative(a_list,item):
-    pass
+def binary_search_iterative(a_list, item):
+  first = 0
+  last = len(a_list) - 1
+  found = False
+
+  while first <= last and not found:
+    midpoint = (first + last) // 2
+    if a_list[midpoint] == item:
+      found = True
+    else: 
+      if item < a_list[midpoint]:
+        last = midpoint - 1
+      else: 
+        first = midpoint + 1
+
+  a_list = a_list.sort()
+
+  return found
     
     
 def binary_search_recursive(a_list,item):
-    pass
+  if len(a_list) == 0:
+    return False
+  else:
+    midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+      return True
+      a_list = a_list.sort()
+    else:
+      if item < a_list[midpoint]:
+        return binary_search_recursive(a_list[:midpoint], item)
+      else:
+        return binary_search_recursive(a_list[midpoint + 1:], item)
+
+
+sequential_search_t1 = Timer('sequential_search(t1, 99999999)', 'from __main__ import sequential_search, t1, random')
+sequential_search_t2 = Timer('sequential_search(t2, 99999999)', 'from __main__ import sequential_search, t2, random')
+sequential_search_t3 = Timer('sequential_search(t3, 99999999)', 'from __main__ import sequential_search, t3, random')
+
+ordered_sequential_search_t1 = Timer('ordered_sequential_search(t1, 99999999)', 'from __main__ import ordered_sequential_search, t1, random')
+ordered_sequential_search_t2 = Timer('ordered_sequential_search(t2, 99999999)', 'from __main__ import ordered_sequential_search, t2, random')
+ordered_sequential_search_t3 = Timer('ordered_sequential_search(t3, 99999999)', 'from __main__ import ordered_sequential_search, t3, random')
+
+binary_search_iterative_t1 = Timer('binary_search_iterative(t1, 99999999)', 'from __main__ import binary_search_iterative, t1, random')
+binary_search_iterative_t2 = Timer('binary_search_iterative(t2, 99999999)', 'from __main__ import binary_search_iterative, t2, random')
+binary_search_iterative_t3 = Timer('binary_search_iterative(t3, 99999999)', 'from __main__ import binary_search_iterative, t3, random')
+
+binary_search_recursive_t1 = Timer('binary_search_recursive(t1, 99999999)', 'from __main__ import binary_search_recursive, t1, random')
+binary_search_recursive_t2 = Timer('binary_search_recursive(t2, 99999999)', 'from __main__ import binary_search_recursive, t2, random')
+binary_search_recursive_t3 = Timer('binary_search_recursive(t3, 99999999)', 'from __main__ import binary_search_recursive, t3, random')
+
+
+def sequential_timer(a_list, item, timer_function):
+  return sequential_search(a_list, item), 'Sequential Search of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+
+def ordered_sequential_timer(a_list, item, timer_function):
+  return ordered_sequential_search(a_list, item), 'Ordered Sequential Search of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+
+def binary_search_iterative_timer(a_list, item, timer_function):
+  return binary_search_iterative(a_list, item), 'Binary Search Iterative of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+
+def binary_search_recursive_timer(a_list, item, timer_function):
+  return binary_search_recursive(a_list, item), 'Binary Search Recursive of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+
+def main():
+  print(sequential_timer(t1, 99999999, sequential_search_t1))
+  print(sequential_timer(t2, 99999999, sequential_search_t2))
+  print(sequential_timer(t3, 99999999, sequential_search_t3))
+  print(ordered_sequential_timer(t1, 99999999, ordered_sequential_search_t1)) 
+  print(ordered_sequential_timer(t2, 99999999, ordered_sequential_search_t2)) 
+  print(ordered_sequential_timer(t3, 99999999, ordered_sequential_search_t3)) 
+  print(binary_search_iterative_timer(t1, 99999999, binary_search_iterative_t1)) 
+  print(binary_search_iterative_timer(t2, 99999999, binary_search_iterative_t2)) 
+  print(binary_search_iterative_timer(t3, 99999999, binary_search_iterative_t3)) 
+  print(binary_search_recursive_timer(t1, 99999999, binary_search_recursive_t1)) 
+  print(binary_search_recursive_timer(t2, 99999999, binary_search_recursive_t2)) 
+  print(binary_search_recursive_timer(t3, 99999999, binary_search_recursive_t3))
 
 
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main()
--- sort_compare.py
+++ sort_compare.py
@@ -1,30 +1,104 @@
 import argparse
-# other imports go here
-
 import random
+import timeit
+from timeit import Timer
+
 
 def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
-    a_list = list(range(n))
-    random.shuffle(a_list)
-    return a_list
+  a_list = random.sample(range(n), 500)
+  random.shuffle(a_list)
+  return a_list
+
+
+t1 = get_me_random_list(500)
+t2 = get_me_random_list(1000)
+t3 = get_me_random_list(5000)
     
 
 def insertion_sort(a_list):
-    pass
+  for index in range(1, len(a_list)):
+
+    current_value = a_list[index]
+    position = index
+
+    while position > 0 and a_list[position - 1] > current_value:
+      a_list[position] = a_list[position - 1]
+      position = position - 1
+
+    a_list[position] = current_value
 
 
 def shell_sort(a_list):
-    pass
+  sublist_count = len(a_list) // 2
+  while sublist_count > 0:
+    for start_position in range(sublist_count):
+      gap_insertion_sort(a_list, start_position, sublist_count)
+
+    #print('After increments of size', sublist_count, 'the list is', a_list)
+    sublist_count = sublist_count // 2
+
+def gap_insertion_sort(a_list, start, gap):
+  for i in range(start + gap, len(a_list), gap):
+    current_value = a_list[i]
+    position = i
+
+    while position >= gap and a_list[position - gap] > current_value:
+      a_list[position] = a_list[position - gap]
+      position = position - gap
+
+  a_list[position] = current_value
 
 
 def python_sort(a_list):
-    pass
+  a_list = a_list.sort()
+  return a_list
+
+
+insertion_sort_t1 = Timer('insertion_sort(t1)', 'from __main__ import insertion_sort, t1, random')
+insertion_sort_t2 = Timer('insertion_sort(t2)', 'from __main__ import insertion_sort, t2, random')
+insertion_sort_t3 = Timer('insertion_sort(t3)', 'from __main__ import insertion_sort, t3, random')
+
+shell_sort_t1 = Timer('shell_sort(t1)', 'from __main__ import shell_sort, t1, random')
+shell_sort_t2 = Timer('shell_sort(t2)', 'from __main__ import shell_sort, t2, random')
+shell_sort_t3 = Timer('shell_sort(t3)', 'from __main__ import shell_sort, t3, random')
+
+python_sort_t1 = Timer('python_sort(t1)', 'from __main__ import python_sort, t1, random')
+python_sort_t2 = Timer('python_sort(t2)', 'from __main__ import python_sort, t2, random')
+python_sort_t3 = Timer('python_sort(t3)', 'from __main__ import python_sort, t3, random')
+
+
+def insertion_sort_timer(a_list, timer_function):
+  return 'Insertion Sort of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+def shell_sort_timer(a_list, timer_function):
+  return 'Shell Sort of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+def python_sort_timer(a_list, timer_function):
+  return 'Python Sort of took %10.7f seconds to run, on average' % (timer_function.timeit(number=100))
+
+
+def main():
+  print(insertion_sort_timer(t1, insertion_sort_t1))
+  print(insertion_sort_timer(t2, insertion_sort_t2))
+  print(insertion_sort_timer(t3, insertion_sort_t3)) 
+  print(shell_sort_timer(t1, shell_sort_t1)) 
+  print(shell_sort_timer(t2, shell_sort_t2)) 
+  print(shell_sort_timer(t3, shell_sort_t3)) 
+  print(python_sort_timer(t1, python_sort_t1)) 
+  print(python_sort_timer(t2, python_sort_t2)) 
+  print(python_sort_timer(t3, python_sort_t3))
+
 
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main()
+
+
+
+
+
+
+
+
